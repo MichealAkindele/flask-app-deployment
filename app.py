d28b1792988dd780e3b1e4a35963b45fa0831b04
Micheal Akindele <micheal.akindele@kibo.school>
2023-10-30 16:48:36 +0100

    answer

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/fizzbuzz")
+@app.route("/")
 
-def fizzbuzz():
-    numbers = []
-
-    for number in range(1,100 + 1):
-        if (number % 3 == 0) and (number % 5 == 0):
-            numbers.append("FizzBuzz")
-        elif number % 3 == 0:
-            numbers.append("Fizz")
-        elif number % 5 == 0:
-            numbers.append("Buzz")
-        else:
-            numbers.append(number)
-
-    return render_template("fizzbuzz.html", numbers=numbers)
+def hello():
+    return "Hello, World"
 
 if __name__ == "__main__":
     app.run()
